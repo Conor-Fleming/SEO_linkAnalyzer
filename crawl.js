c09f4871da933fc4e54dffa65d30a8e82dd517ed
Conor Fleming <conorfleming03@gmail.com>
2022-10-12 17:56:15 -0700

    tests running successfully(failing) now? strange.

--- crawl.js
+++ crawl.js
@@ -1,11 +1,8 @@
-import url from 'node:url';
-
 function normalizeURL(url) {
     const myURL = url.parse(url)
     return myURL
 }
 
-
-modules.exports = {
+module.exports = {
     normalizeURL
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
-const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
 test('takes input URL and normalizes', () => {
     expect(normalizeURL("https://test.com/dev").toBe("test.com/dev"));

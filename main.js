2c462da0596670e645530a516ccca9423ba63d28
Conor Fleming <conorfleming03@gmail.com>
2022-10-14 20:32:40 -0700

    starting main func

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+function main() {
+    const args = process.argv.slice(2)
+    if (args.length < 1 || args.length > 1) {
+        console.log("incorrect number of arguments...exiting")
+        return
+    }
+    console.log("Crawling Site...")
+}
+
+main()
